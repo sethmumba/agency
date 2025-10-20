5ecdb6180e635fb09aada73a5c852f36c4f515a8
unknown <Seth Mumba>
2025-10-21 00:52:11 +0300

    2

--- inx/models.py
+++ inx/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Service(models.Model):
+class Portfolio(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='services/', blank=True, null=True)
--- inx/views.py
+++ inx/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import Service
+from .models import Portfolio
 
 # Create your views here.
 def home(request):
-    services = Service.objects.all()
-    return render(request, 'inx/index.html')
+    portfolio = Portfolio.objects.all()
+    return render(request, 'inx/index.html', {'portfolio': portfolio})
